8f0e4577cdc33975096d4a3b27ee219c1f193001
Glitch (glitch-hello-website) <none>
2021-11-22 04:24:40 +0000

    🙎🏵 Checkpoint ./apps/chat/app.js:38399113/3540

--- apps/chat/app.js
+++ apps/chat/app.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyAxAqkFw8oIJiHyLAQllDk4W9bh3pbBC4g",
-  authDomain: "unblocked-infinite-a9e25.firebaseapp.com",
-  databaseURL: "https://unblocked-infinite-a9e25-default-rtdb.firebaseio.com",
-  projectId: "unblocked-infinite-a9e25",
-  storageBucket: "unblocked-infinite-a9e25.appspot.com",
-  messagingSenderId: "267045856766",
-  appId: "1:267045856766:web:baa6dc5285d5dcf322ccaf"
+    apiKey: "AIzaSyAxAqkFw8oIJiHyLAQllDk4W9bh3pbBC4g",
+    authDomain: "https://unblocked-infinite-a9e25-default-rtdb.firebaseio.com",
+    databaseURL: "https://unblocked-infinite-a9e25-default-rtdb.firebaseio.com",
+    projectId: "unblocked-infinite-a9e25",
+    storageBucket: "unblocked-infinite-a9e25.appspot.com",
+    messagingSenderId: "unblocked-infinite-a9e25.appspot.com",
+    appId: "1:267045856766:web:baa6dc5285d5dcf322ccaf"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -20,11 +20,14 @@ document.getElementById('file').addEventListener('change', (event) => {
         console.log(progress);
         const progressBar = document.getElementById('progress_bar');
         progressBar.value = progress;
+
     });
 
     storageRef.getDownloadURL().then(function(url){
+console.log(storageRef.getDownloadURL())
         const image = document.getElementById('image');
         console.log(url);
         image.src = url
     });
 });
+
