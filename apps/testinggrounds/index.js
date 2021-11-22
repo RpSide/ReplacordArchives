eb78ba9e81a7bd08aa07b527115ade426b7a3a5b
Glitch (glitch-hello-website) <none>
2021-11-22 20:01:51 +0000

    🎰🛣 Checkpoint ./apps/testinggrounds/index.js:38399113/32118 ./apps/testinggrounds/index.html:38399113/2

--- apps/testinggrounds/index.js
+++ apps/testinggrounds/index.js
@@ -16,64 +16,10 @@ var room = "f/"
   firebase.initializeApp(firebaseConfig);
 
 
-  var db = firebase.database();
+ var database = firebase.database();
 
-function d(params){
-let roomcode = prompt("chatroom code", room);
-let text;
-if (roomcode == null || roomcode == "") {
- alert ('empty field');
-} else {
-  text = roomcode;
-
-let nm = prompt("chatroom name", '');
-let text;
-if (nm == null || nm == "") {
- alert ('empty field');
-} else {
-  text = nm;
-
-        var e = db.ref('e/');
-      e.once("value", function(snapshot) {
-        var index = parseFloat(snapshot.numChildren()) + 1;
-        db.ref('e/')
-          .set({
-            name:nm
-          })
-      });
-
-var e = db.ref('e/');
-      e.on("value", function(snapshot) {
-        if (snapshot.numChildren() == 0) {
-          return;
-        }
-        var values = Object.values(snapshot.val());
-        var guide = [];
-        var unordered = [];
-        var ordered = [];
-        for (var i, i = 0; i < values.length; i++) {
-          guide.push(i + 1);
-          unordered.push([values[i], values[i].index]);
-        }
-
-        guide.forEach(function(key) {
-          var found = false;
-          unordered = unordered.filter(function(item) {
-            if (!found && item[1] == key) {
-              ordered.push(item[0]);
-              found = true;
-              return false;
-            } else {
-              return true;
-            }
-          });
-        });
-
-        ordered.forEach(function(data) {
-          var name = data.name;
-        })})};
-
-
-window.location.replace( 'https://devcompessay.glitch.me?appID=3&code=' + roomcode)
-}
+function writeUserData(chatroomcode, chatroomname) {
+  database.ref(chatroomcode + '/').set({
+    name: chatroomname,
+  });
 }
