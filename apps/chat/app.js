7592fbf75918de47c0b6c47680c497a7b8e6bc4d
Glitch (glitch-hello-website) <none>
2021-11-22 02:34:02 +0000

    🎫⛲️ Checkpoint ./apps/chat/indexx.html:38399113/7668 ./apps/chat/app.js:38399113/15045

---
+++ apps/chat/app.js
@@ -0,0 +1,30 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAxAqkFw8oIJiHyLAQllDk4W9bh3pbBC4g",
+  authDomain: "unblocked-infinite-a9e25.firebaseapp.com",
+  databaseURL: "https://unblocked-infinite-a9e25-default-rtdb.firebaseio.com",
+  projectId: "unblocked-infinite-a9e25",
+  storageBucket: "unblocked-infinite-a9e25.appspot.com",
+  messagingSenderId: "267045856766",
+  appId: "1:267045856766:web:baa6dc5285d5dcf322ccaf"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+document.getElementById('file').addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    const storageRef = firebase.storage().ref('images/' + file.name);
+
+    storageRef.put(file).on('state_changed', (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(progress);
+        const progressBar = document.getElementById('progress_bar');
+        progressBar.value = progress;
+    });
+
+    storageRef.getDownloadURL().then(function(url){
+        const image = document.getElementById('image');
+        console.log(url);
+        image.src = url
+    });
+});
