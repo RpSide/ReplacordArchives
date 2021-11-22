92cd6cc89225d71fd074d75904b8872bfaa67f07
Glitch (glitch-hello-website) <none>
2021-11-22 01:58:35 +0000

    🍫🍱 Checkpoint ./apps/chat/index.css:38399113/12498 ./apps/chat/index.js:38399113/163

--- apps/chat/index.js
+++ apps/chat/index.js
@@ -319,6 +319,9 @@ get_url() {
       if (localStorage.getItem("url") != null) {
         return localStorage.getItem("url");
       } else {
+localStorage.setItem("url", 'https://th.bing.com/th/id/OIP.gDBdAA5pacZzSnpFUHEKPgHaGA?w=246&h=199&c=7&r=0&o=5&pid=1.7') != null
+
+localStorage.getItem("url")
         this.home();
       }
     }
