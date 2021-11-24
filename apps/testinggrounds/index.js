e17dd5f813d1506bf43a10d5404cbfa9cde89a50
Glitch (cliff-topaz-pencil) <none>
2021-11-24 02:21:11 +0000

    🍿🖖 Checkpoint ./apps/testinggrounds/chat/index.css:38399113/19 ./apps/testinggrounds/index.js:38399113/5316 ./apps/testinggrounds/index.html:38399113/32

--- apps/testinggrounds/index.js
+++ apps/testinggrounds/index.js
@@ -1,6 +1,4 @@
-var room = "f/"
-
-  // Your web app's Firebase configuration
+ // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCF46UDRHhke3cHfUFOZ3YNcq9EJWdC10Y",
@@ -23,55 +21,43 @@ function makenormalroom() {
 
  var roomcode = Math.floor(Math.random() * 8) + 1 + "" + Math.floor(Math.random() * 8) + 1 + "" + Math.floor(Math.random() * 8) + 1 + "" + Math.floor(Math.random() * 8) + 1 + "" + Math.floor(Math.random() * 8) + 1 + "" + Math.floor(Math.random() * 8) + 1 + "" + Math.floor(Math.random() * 8) + 1 + "" + Math.floor(Math.random() * 8) + 1 + "" + Math.floor(Math.random() * 8) + 1 + "" + Math.floor(Math.random() * 8) + 1
   
-let roomname = prompt("chatroom name", room);
+let roomname = prompt("chatroom name", '');
 let text2;
 if (roomname == null || roomname == "") {
  alert ('empty field');
 } else {
-database.ref(roomcode + '/').set({
+firebase.database().ref(roomcode + '/').set({
     name: roomname,
   });
 
 
 
 
-function makespecialroom() {
+function makespecialroom(params) {
   
-let roomcode = prompt("chatroom code", room);
+let roomname = prompt("chatroom name", '');
+let text2;
+if (roomname == null || roomname == "") {
+ alert ('empty field');
+} else {
+let roomcode = prompt("chatroom code", '');
 let text2;
 if (roomcode == null || roomcode == "") {
  alert ('empty field');
 } else {
-let roomname = prompt("chatroom name", room);
-let text2;
-if (roomname == null || roomname == "") {
- alert ('empty field');
-} else {
-database.ref(roomcode + '/').set({
+firebase.database().ref(roomcode + '/').set({
     name: roomname,
   });
-};
-
-
-
-
-dbRef.child(roomcode).child('name').get().then((snapshot) => {
-  if (snapshot.exists()) {
-    console.log(snapshot.val());
-  } else {
-    console.log("No data available");
-  }
-}).catch((error) => {
-  console.error(error);
-});
-
+}
+}
+}
 }
 }
 
 
 function writeUserData() {
   
-let roomname = prompt("chatroom name", room);
+let roomname = prompt("chatroom name", '');
 let text2;
 if (roomname == null || roomname == "") {
  alert ('empty field');
@@ -79,17 +65,7 @@ if (roomname == null || roomname == "") {
 database.ref(roomcode + '/').set({
     name: roomname,
   });
-
-
-dbRef.child(roomcode).child('name').get().then((snapshot) => {
-  if (snapshot.exists()) {
-    console.log(snapshot.val());
-  } else {
-    console.log("No data available");
-  }
-}).catch((error) => {
-  console.error(error);
-});
-
 }
+}
+
 }
