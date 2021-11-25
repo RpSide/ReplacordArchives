4fb54bafb837050ad9bfa402046ed9abb8a06892
Glitch (cliff-topaz-pencil) <none>
2021-11-25 02:40:40 +0000

    👢👂 Checkpoint ./apps/testinggrounds/chat/index.js:38399113/1015

--- apps/testinggrounds/chat/index.js
+++ apps/testinggrounds/chat/index.js
@@ -35,7 +35,97 @@ const firebaseConfig = {
   create_join_form(){
       // YOU MUST HAVE (PARENT = THIS). OR NOT. I'M NOT YOUR BOSS!😂
       var parent = this;
+// sign up, in, and out
+const auth = firebase.auth();
+	
+	
+	function signUp(){
+		
+		var email = document.getElementById("email");
+		var password = document.getElementById("password");
+		
+		const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+		promise.catch(e => alert(e.message));
+		
+		alert("Signed Up");
+	}
+	
+	
+	
+	function signIn(){
+		
+		var email = document.getElementById("email");
+		var password = document.getElementById("password");
+		
+		const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+		promise.catch(e => alert(e.message));
+		
+		
+		
+		
+	}
+	
+	
+	function signOut(){
+		
+		auth.signOut();
+		alert("Signed Out");
+		
+	}
+	
+	
+	
+	auth.onAuthStateChanged(function(user){
+		
+		if(user){
+			
+			var email = user.email;
+			alert("Active User " + email);
+			
+			//Take user to a different or home page
 
+			//is signed in
+			
+		}else{
+			
+			alert("No Active User");
+			//no user is signed in
+		}
+		
+		
+		
+	});
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
       var join_container = document.createElement('div')
       join_container.setAttribute('id', 'join_container')
       var join_inner_container = document.createElement('div')
