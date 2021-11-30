d59c96ae37d4ebf56a9969eeeba3a2c6d14a6b8c
Glitch (devcompessays) <none>
2021-11-30 03:00:23 +0000

    🏄👐 Checkpoint ./apps/testinggrounds/fileuploads/app.js:38399113/1733

--- apps/testinggrounds/fileuploads/app.js
+++ apps/testinggrounds/fileuploads/app.js
@@ -12,35 +12,21 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-// Create a root reference
-var storageRef = firebase.storage().ref();
 
 document.getElementById('file').addEventListener('change', (event) => {
     const file = event.target.files[0];
-    const storageRef = firebase.storage().ref('user-pfps/' + file.name);
+    const storageRef = firebase.storage().ref('images/' + file.name);
 
     storageRef.put(file).on('state_changed', (snapshot) => {
         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         console.log(progress);
         const progressBar = document.getElementById('progress_bar');
         progressBar.value = progress;
-
-    });
- storageRef.getDownloadURL().then(function(url){
-
-console.log(storageRef.getDownloadURL())
-        const image = document.getElementById('image');
-        console.log(url)
-image.src = url
-alert(url)
     });
 
-storageRef.getDownloadURL().then(function(url){
-
-console.log(storageRef.getDownloadURL())
+    storageRef.getDownloadURL().then(function(url){
         const image = document.getElementById('image');
-        console.log(url)
-image.src = url
-alert(url)
+        console.log(url);
+        image.src = url
     });
 });
