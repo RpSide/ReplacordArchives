290e7d5b692b28d796b9c36f15d107e0e77b00bb
Glitch (devcompessays) <none>
2021-12-06 19:05:30 +0000

    🚡🐂 Checkpoint ./apps/testinggrounds/fileuploads/app.js:38399113/9023 ./apps/testinggrounds/fileuploads/index.html:38399113/1689

--- apps/testinggrounds/fileuploads/app.js
+++ apps/testinggrounds/fileuploads/app.js
@@ -1,12 +1,13 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyCF46UDRHhke3cHfUFOZ3YNcq9EJWdC10Y",
-    authDomain: "test-6dc98.firebaseapp.com",
-    databaseURL: "https://chat-883eb-default-rtdb.firebaseio.com",
-    projectId: "test-6dc98",
-    storageBucket: "chat-883eb.firebaseapp.com",
-    messagingSenderId: "710439854114",
-    appId: "1:710439854114:web:11da79de037bfe857a2a7b"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCF46UDRHhke3cHfUFOZ3YNcq9EJWdC10Y",
+  authDomain: "chat-883eb.firebaseapp.com",
+  databaseURL: "https://chat-883eb-default-rtdb.firebaseio.com",
+  projectId: "chat-883eb",
+  storageBucket: "chat-883eb.appspot.com",
+  messagingSenderId: "421817736954",
+  appId: "1:421817736954:web:32ac95e1f698ea3ff0f343",
+  measurementId: "G-YCQZGGEGZ9"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
