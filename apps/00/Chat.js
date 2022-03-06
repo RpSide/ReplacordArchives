29e5fd81d299e48c4ee8ad456435a48452df9dd9
Zombiefiedgamingyt <93808177+Zombiefiedgamingyt@users.noreply.github.com>
2022-03-05 23:36:36 -0600

    Update Chat.js

--- apps/00/Chat.js
+++ apps/00/Chat.js
@@ -323,7 +323,7 @@ var time = (dateee.getHours()) + ':' + dateee.getMinutes() + ' am'
         var index = parseFloat(snapshot.numChildren()) + 1;
         db.ref('chats/' + room + 'messages/' + messageID)
           .set({
-            profilepic: 'https://proxy-copy.glitch.me/' + parent.get_url(),
+            profilepic: parent.get_url(),
             name: parent.get_name(),
             message: message,
             color: parent.get_color(),
